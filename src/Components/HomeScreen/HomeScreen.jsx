02a227e5fdeb1a451a38edd34470dc753e9dc44c
MichaelStarkman <michaelstarkman@gmail.com>
2022-05-23 23:30:34 -0600

    navbar and hero

---
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <h3>Hello, I am</h3>
+            <h1>Michael Starkman</h1>
+            {/* Insert Typist here */}
+            <h2>Software Engineer</h2>
+            <h2>Bootcamp Graduate</h2>
+            <h2>Web Developer</h2>
+            <h2>Hawkeye</h2>
+        </div>
+    )
+}
+export default HomeScreen;
---
+++ src/Components/PortNavBar/PortNavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+
+const PortNavBar = () => {
+    return (
+        <>
+            <div className="NavBar">
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#home">Michael Starkman</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Portfolio</Nav.Link>
+                            <Nav.Link href="#link">About Me</Nav.Link>
+                            <Nav.Link href="#link">Contact</Nav.Link>  
+                            {/* might make it into a button on home page */}
+                            <Nav.Link href="#link">Resume</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        </>    
+    )
+}
+export default PortNavBar;
