65ee38c72e655dfe63a262de14438c56bbfa3d0f
MichaelStarkman <michaelstarkman@gmail.com>
2022-06-01 15:33:45 -0600

    contact styled mobile

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -21,14 +21,19 @@ const ContactForm = () => {
     <div className='contact-container'>
       <div>
         <h1 className='contact-title'>Contact</h1>
-        <p>I enjoy interacting with people from all facets of life. Let's link up over Zoom, a phone call, or coffee if you are in the Denver area.</p>
+        <hr className='contact-hr' />
+        <p className='contact-text'>I enjoy interacting with people from all facets of life. Let's link up over Zoom, a phone call, or coffee if you are in the Denver area.</p>
         <SocialIcons></SocialIcons>
-        <p>Send me a message:</p>
+        <br />
+        <p className='contact-text'>Send me a message:</p>
     </div>
       <div className='form-container'>
-        <form onSubmit={handleSubmit}>
+        <form 
+        className='form'
+        onSubmit={handleSubmit}>
         {/* Name */}
         <input
+          className="input"
           id="name"
           type="name" 
           name="name"
@@ -43,6 +48,7 @@ const ContactForm = () => {
         <br />
         {/* email */}
         <input
+          className="input"
           id="email"
           type="email" 
           name="email"
@@ -71,6 +77,7 @@ const ContactForm = () => {
         <br /> */}
         {/* subject */}
         <input
+          className="input"
           id="subject"
           type="subject" 
           name="subject"
@@ -83,6 +90,8 @@ const ContactForm = () => {
           Message: 
         </label> */}
         <textarea
+          className="w-75 mb-4"
+          rows={5}
           id="message"
           name="message"
           required
@@ -93,10 +102,10 @@ const ContactForm = () => {
           field="message"
           errors={state.errors}
         />
-        <br />
+        <br /><br />
         {/* TODO: possible preferred contact drop down text/phone/email */}
           <Button 
-          variant='dark'
+          variant='outline-dark'
           type="submit" 
           disabled={state.submitting} 
           // onClick={() => setSmShow(true)} 
